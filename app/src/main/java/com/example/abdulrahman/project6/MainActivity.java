6bd6e186f71d00750fc0456a2d627a2bb33718ea
unknown <abdxax@gmail.com>
2018-04-28 19:13:25 +0300

    all bugs is fixed

--- app/src/main/java/com/example/abdulrahman/project6/MainActivity.java
+++ app/src/main/java/com/example/abdulrahman/project6/MainActivity.java
@@ -1,21 +1,15 @@
 package com.example.abdulrahman.project6;
-
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
